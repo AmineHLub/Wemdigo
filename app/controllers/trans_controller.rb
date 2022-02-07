f69039ef42e6e0d86df5a9c7790b4defdd8dee8c
AmineHLub <Amino.bhl@gmail.com>
2022-02-07 07:46:05 +0100

    Tweaks at trans controller

--- app/controllers/trans_controller.rb
+++ app/controllers/trans_controller.rb
@@ -17,14 +17,13 @@ class TransController < ApplicationController
 
   def create
     @tran = Tran.new(tran_params)
-
+    @tran.user_id = current_user.id
+    @tran.category_id = params[:category_id]
     respond_to do |format|
       if @tran.save
         format.html { redirect_to tran_url(@tran), notice: "Tran was successfully created." }
-        format.json { render :show, status: :created, location: @tran }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @tran.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -33,10 +32,8 @@ class TransController < ApplicationController
     respond_to do |format|
       if @tran.update(tran_params)
         format.html { redirect_to tran_url(@tran), notice: "Tran was successfully updated." }
-        format.json { render :show, status: :ok, location: @tran }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @tran.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -46,7 +43,6 @@ class TransController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to trans_url, notice: "Tran was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
@@ -55,7 +51,6 @@ class TransController < ApplicationController
       @tran = Tran.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def tran_params
       params.require(:tran).permit(:name, :amount)
     end
