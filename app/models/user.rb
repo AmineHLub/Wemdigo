c879233b6fb630d3945e2d95401a841bec9d1170
AmineHLub <Amino.bhl@gmail.com>
2022-02-07 05:41:22 +0100

    Add user_ref and route changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
 
-  has_many :foods
-  has_many :recipes
+  has_many :categories
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories
   devise_for :users
-
   devise_scope :user do
     root 'users#index'
   resources :users, only: [:index]
   end
+  resources :categories
 end
