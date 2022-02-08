e3582af4b7149f33d8027bf26828e6415528ab94
AmineHLub <Amino.bhl@gmail.com>
2022-02-08 13:03:31 +0100

    styling categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
-    root 'users#index'
+    root 'categories#index'
     get '/users/sign_out' => 'devise/sessions#destroy'
   resources :users, only: [:index]
   end
