7ba64c36bbe054eb0ab3a6badaac45274f542ad0
AmineHLub <Amino.bhl@gmail.com>
2022-02-08 07:47:24 +0100

    Add navbar and burger menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery prepend: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     root 'users#index'
+    get '/users/sign_out' => 'devise/sessions#destroy'
   resources :users, only: [:index]
   end
   resources :categories
